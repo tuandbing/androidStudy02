9344f80085ef5e77f9c5c2f108ed83f8dccf8762
tuandbing <1873656423@qq.com>
2024-07-10 22:21:02 +0800

    添加了一些基本的布局介绍代码

--- BaseProject/app/src/main/java/com/example/baseproject/MainActivity2.java
+++ BaseProject/app/src/main/java/com/example/baseproject/MainActivity2.java
@@ -1,6 +1,11 @@
 package com.example.baseproject;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,5 +20,32 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main2);
+
+        // 在java代码中实现视图的宽高设置
+        // 获取文本视图
+        TextView textView = findViewById(R.id.tv2);
+
+        // 获取textView布局参数
+        ViewGroup.LayoutParams layoutParams = textView.getLayoutParams();
+
+        // 修改宽度
+        layoutParams.width = MainActivity2.dip2px(this, 300);
+
+        textView.setBackgroundColor(Color.GREEN);
+
+        textView.setText("hello world");
+
+        textView.setLayoutParams(layoutParams);
+
+
     }
+
+    public static int dip2px(Context context, float dpValue){
+
+        // 获取当前手机的像素密度
+        float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);	// 四舍五入取整
+
+    }
+
 }
