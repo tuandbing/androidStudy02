190771db9cb2254ea48462b3fbe82323cd2d876c
tuandbing <1873656423@qq.com>
2024-08-26 21:44:30 +0800

    add

--- chapter07/app/src/main/java/com/example/chapter07/MainActivity.java
+++ chapter07/app/src/main/java/com/example/chapter07/MainActivity.java
@@ -32,6 +32,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 /*
 public class MainActivity extends AppCompatActivity {
@@ -323,76 +324,23 @@ public class MainActivity extends AppCompatActivity {
 */
 
 public class MainActivity extends AppCompatActivity {
-
-
-    private List<String> tabList;
-
-    private List<View> pageList;
-
-    private View view1, view2, view3, view4;
-
-    private PagerAdapter adapter = new PagerAdapter() {
-
-        @Override
-        public int getCount() {
-            return pageList != null ? pageList.size() : 0;
-        }
-
-        @Override
-        public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
-            return view == object;
-        }
-
-        @NonNull
-        @Override
-        public Object instantiateItem(@NonNull ViewGroup container, int position) {
-            container.addView(pageList.get(position));
-            return pageList.get(position);
-        }
-
-        @Override
-        public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
-            container.removeView(pageList.get(position));
-        }
-
-        @Nullable
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return tabList.get(position);
-        }
-    };
-
+    private TabHost tabHost;        //声明TabHost组件的对象
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main04);
-        ViewPager viewPager = findViewById(R.id.viewPager);
-        tabList = new ArrayList<>();
-        pageList = new ArrayList<>();
-        /*view1 = LayoutInflater.from(getApplication()).inflate(R.layout.layout1, null);
-        view2 = LayoutInflater.from(getApplication()).inflate(R.layout.layout2, null);
-        view3 = LayoutInflater.from(getApplication()).inflate(R.layout.layout3, null);
-        view4 = LayoutInflater.from(getApplication()).inflate(R.layout.layout4, null);*/
-
-        // 两种方法都可以获取LayoutInflater对象
-        view1 = getLayoutInflater().inflate(R.layout.layout1, null);
-        view2 = getLayoutInflater().inflate(R.layout.layout2, null);
-        view3 = getLayoutInflater().inflate(R.layout.layout3, null);
-        view4 = getLayoutInflater().inflate(R.layout.layout4, null);
-
-        pageList.add(view1);
-        pageList.add(view2);
-        pageList.add(view3);
-        pageList.add(view4);
-
-        tabList.add("毕业格言1");
-        tabList.add("毕业格言2");
-        tabList.add("毕业格言3");
-        tabList.add("毕业格言4");
-
-        viewPager.setAdapter(adapter);
-
-
+        setContentView(R.layout.activity_main06);
+        tabHost=(TabHost)findViewById(android.R.id.tabhost);    //获取TabHost对象
+        tabHost.setup();        //初始化TabHost组件
+        // 声明并实例化一个LayoutInflater对象
+        LayoutInflater inflater = LayoutInflater.from(this);
+        inflater.inflate(R.layout.tab1, tabHost.getTabContentView());
+        inflater.inflate(R.layout.tab2,tabHost.getTabContentView());
+        tabHost.addTab(tabHost.newTabSpec("tab1")
+                .setIndicator("精选表情")
+                .setContent(R.id.linearlayout1));             //添加第一个标签页
+        tabHost.addTab(tabHost.newTabSpec("tab2")
+                .setIndicator("投稿表情")
+                .setContent(R.id.framelayout)) ;              //添加第二个标签页
     }
-}
 
+}
